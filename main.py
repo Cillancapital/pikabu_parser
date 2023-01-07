37e1cce576ad38ff7ceccb7030177a9c3c5a4934
cillancapital <cillancapital2@gmail.com>
2023-01-08 01:11:10 +0300

    GUI v1 done

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
+import tkinter as tk
+from tkinter.ttk import Combobox
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        #configure the root window
+        self.geometry('350x600')
+        self.minsize(350, 600)
+        self.title('Welcome to Pikabu parser')
+
+        #configure content
+        self.lbl1 = tk.Label(self, text = "Chose your destiny",font = ('Arial',18))
+        self.lbl1.place(anchor='center', relx=0.5, rely=0.1)
+
+        self.combo1 = Combobox(self)
+        self.combo1.place(anchor='center', relx=0.5, rely=0.18)
+        self.combo1['values'] = ('Parse from hot','doesnt work')
+        self.combo1.current(0)
+
+        self.combo2 = Combobox(self)
+        self.combo2.place(anchor='center', relx=0.5, rely=0.22)
+        self.combo2['values'] = tuple([f'Parse {i} pages' for i in range(1,100)])
+        self.combo2.current(0)
+
+        self.btn1 = tk.Button(self, text ="Let's parse!", command=self.btn1_clicked)
+        self.btn1.place(anchor='center', relx=0.5, rely=0.3)
+
+        #Todo add a funny image
+
+    def btn1_clicked(self):
+        self.lbl2 = tk.Label(self, text="Done!", font=('Arial', 18))
+        self.lbl2.place(anchor='center', relx=0.5, rely=0.6)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = App()
+    app.mainloop()
 
 
